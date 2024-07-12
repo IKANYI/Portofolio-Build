63b63c11af63e4c4103ac6a0a83781bb087ca9ab
IKANYI <paulkaranja214@gmail.com>
2024-07-12 21:56:45 +0300

    create the post section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import  profileImg from '../assets/simons.jpg';
+import  dashImg from '../assets/dash.jpg';
+import  portImg from '../assets/portrait.jpg';
+import  mayaImg from '../assets/maya.jpg';
 import './main.css';
 function Home() {
   return (
@@ -28,6 +31,39 @@ function Home() {
             <h2>Creating pixel perfect icons in Figma</h2>
             <h4>12 Feb 2020   |  Figma,Icon Design</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illum quam consectetur. Odio nesciunt quam accusantium quis excepturi aliquid nemo.</p>
+          </div>
+        </div>
+      </div>
+      <div className="featured-work">
+        <h3>Featured Works</h3>
+        <div className="works">
+          <div className="works-img">
+            <img src={dashImg} alt="work thumbnail" />
+          </div>
+          <div className="works-text">
+            <h3>Designing Dashboards</h3>
+            <div className='works-date'><h4>2020</h4><p>Dashboard</p></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
+          </div>
+        </div>
+        <div className="works">
+          <div className="works-img">
+            <img src={portImg} alt="work thumbnail" />
+          </div>
+          <div className="works-text">
+            <h3>Vibrant Portraits of 2020</h3>
+            <div className='works-date'><h4>2018</h4><p>Illustration</p></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
+          </div>
+        </div>
+        <div className="works">
+          <div className="works-img">
+            <img src={mayaImg} alt="work thumbnail" />
+          </div>
+          <div className="works-text">
+            <h3>36 days of Malayalam type</h3>
+            <div className='works-date'><h4>2018</h4><p>Typography</p></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
           </div>
         </div>
       </div>
