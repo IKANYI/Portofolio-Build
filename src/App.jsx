f1cf60ee4792dcdb8ad8eedb2e5485a9f181fddb
IKANYI <paulkaranja214@gmail.com>
2024-07-12 22:40:06 +0300

    add a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.css'
 import Head from './components/Head.jsx'
 import Home from './pages/Home.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <>
-      <Head/>
-      <Home/>
+      <BrowserRouter>
+        <Head/>
+        <Routes>
+           <Route path="/" element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+     
       
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import './Head.css';
+function Footer( ){
+  return(
+    <div className="main">
+      <div className="socials">
+        <a href=""><FaFacebookSquare /></a>
+        <a href=""><FaInstagram /></a>
+        <a href=""><FaTwitter /></a>
+        <a href=""><FaLinkedin /></a>
+      </div>
+      <div className="copy">
+        <p>Copyright &copy;2020  All rights reserved</p>
+      </div>
+    </div>
+  )
+}
+export default Footer
