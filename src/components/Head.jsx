f474005cd26cfd7cb803bafa7bb3d84b3c75c0f0
IKANYI <paulkaranja214@gmail.com>
2024-07-15 19:45:00 +0300

    create menu bar

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
-import "./Head.css";
-function Head() {
-  return (
-    <div className="header">
-      <div className="links">
-      
-      <Link to="/"></Link>
-      <Link to="/Blogs">Blog</Link>
-      <Link to="/works">Works</Link>
-      <Link to="/contact">Contact</Link>
-      
-      </div>
-    </div>
-  );
+import { useState } from 'react'
+import './Head.css'
+import { Link } from "react-router-dom"
+import { FaBars } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
+
+function Header() {
+    const [menu, setMenu] = useState("Home")
+    const [click, setClick] = useState(true)
+
+    const handleMenu = () => setClick(!click)
+
+    return (
+        <header>
+            <div className="menu-navbar">
+                <Link to='/'><li onClick={() => { setMenu("Home") }}>Home{menu === "Home" ? <hr /> : <></>}</li></Link>
+                <Link to='/Blogs' onClick={() => { setMenu("Blogs") }}><li>Blog{menu === "Blogs" ? <hr /> : <></>}</li></Link>
+                <Link to='/Work'><li onClick={() => { setMenu("Work") }}>Work{menu === "Work" ? <hr /> : <></>}</li></Link>
+            </div>
+            {click ? <div className="menu-nav-bar">
+                <div className="menu">
+                    <h2 onClick={handleMenu}><IoMdClose /></h2>
+                </div>
+                <Link to='/'><li onClick={() => { setMenu("Home") }}>Home{menu === "Home" ? <hr /> : <></>}</li></Link>
+                <Link to='/Blogs' onClick={() => { setMenu("Blogs") }}><li>Blog{menu === "Blogs" ? <hr /> : <></>}</li></Link>
+                <Link to='/Work'><li onClick={() => { setMenu("Work") }}>Work{menu === "Work" ? <hr /> : <></>}</li></Link>
+            </div> : <div className="menu">
+                <h2 onClick={handleMenu}><FaBars /></h2>
+            </div>}
+        </header>
+    )
 }
-export default Head;
+
+export default Header
