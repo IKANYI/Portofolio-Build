033bd965eadf8f189b982709826b206b19a491ee
Your name <your_email@example.com>
2024-07-15 14:59:32 +0300

    create works and blog page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.css'
 import Head from './components/Head.jsx'
 import Home from './pages/Home.jsx'
+import Work from './pages/Work/Work.jsx'
+import Blogs from './pages/Blogs/Blog.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Head/>
         <Routes>
            <Route path="/" element={<Home/>}/>
+           <Route path="/works" element={<Work/>}/>
+           <Route path="/blogs" element={<Blogs/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
---
+++ src/pages/Work/Work.jsx
@@ -0,0 +1,64 @@
+
+import carolynImg from "../../assets/carolyn.jpg"
+import "./Work.css";
+function Work() {
+  return (
+    <div className="workcontainer">
+      <div className="worktitle"><h1>Work</h1></div>
+      <div className="cards">
+      <div className="workcontent">
+        <div className="work-image">
+          <img src={carolynImg} alt="" />
+        </div>
+        <div className="workcontentleft">
+          <div className="workcontentlefttitle">
+            <h2>Designing Dashboards</h2>
+          <button>2020</button>    <span>Dashboard</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, culpa repudiandae accusamus nam dolore adipisci rerum praesentium architecto? Eum quas provident qui eveniet, ipsa ratione saepe modi? Laboriosam, soluta vero.</p>
+          </div>
+        </div>
+      </div>
+      <div className="workcontent">
+        <div className="work-image">
+          <img src={carolynImg} alt="" />
+        </div>
+        <div className="workcontentleft">
+          <div className="workcontentlefttitle">
+            <h2>Designing Dashboards</h2>
+          <button>2020</button>    <span>Dashboard</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, culpa repudiandae accusamus nam dolore adipisci rerum praesentium architecto? Eum quas provident qui eveniet, ipsa ratione saepe modi? Laboriosam, soluta vero.</p>
+          </div>
+        </div>
+      </div>
+      <div className="workcontent">
+        <div className="work-image">
+          <img src={carolynImg} alt="" />
+        </div>
+        <div className="workcontentleft">
+          <div className="workcontentlefttitle">
+            <h2>Designing Dashboards</h2>
+          <button>2020</button>    <span>Dashboard</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, culpa repudiandae accusamus nam dolore adipisci rerum praesentium architecto? Eum quas provident qui eveniet, ipsa ratione saepe modi? Laboriosam, soluta vero.</p>
+          </div>
+        </div>
+      </div>
+      <div className="workcontent">
+        <div className="work-image">
+          <img src={carolynImg} alt="" />
+        </div>
+        <div className="workcontentleft">
+          <div className="workcontentlefttitle">
+            <h2>Designing Dashboards</h2>
+          <button>2020</button>    <span>Dashboard</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, culpa repudiandae accusamus nam dolore adipisci rerum praesentium architecto? Eum quas provident qui eveniet, ipsa ratione saepe modi? Laboriosam, soluta vero.</p>
+          </div>
+        </div>
+      </div> 
+      </div>
+      
+      
+    </div>
+  );
+}
+
+export default Work;
