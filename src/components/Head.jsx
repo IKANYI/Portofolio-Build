ad282fa69384d07fd8c25be076bb6e850d615358
Manifest-Son <lennoxgithinji37@gmail.com>
2024-07-15 19:38:02 +0300

    It setup the testing environment

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 import "./Head.css";
+
 function Head() {
   return (
     <div className="header">
       <div className="links">
-      
-      <Link to="/"></Link>
-      <Link to="/Blogs">Blog</Link>
-      <Link to="/works">Works</Link>
-      <Link to="/contact">Contact</Link>
-      
+        <Link to="/"></Link>
+        <h1>
+          <Link to="/Blogs">Blog</Link>
+        </h1>
+        <h1>
+          <Link to="/works">Works</Link>
+        </h1>
+        <h1>
+          <Link to="/contact">Contact</Link>
+        </h1>
       </div>
     </div>
   );
